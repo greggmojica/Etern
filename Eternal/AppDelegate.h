6abf97bd8c3aaae463c2fa3a2f5eb67d637cb751
Gregg Mojica <gwmojica@gmail.com>
2015-07-15 11:07:41 -0400

    Initial Commit

---
+++ Eternal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Eternal
+//
+//  Created by Gregg Mojica on 7/15/15.
+//  Copyright (c) 2015 Gregg Mojica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Eternal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Eternal
+//
+//  Created by Gregg Mojica on 7/15/15.
+//  Copyright (c) 2015 Gregg Mojica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
